037ca66ab92507ae07262eb6d7ff708041e08b5e
Yonatan-shitrit <jonatan.shitrit@gmail.com>
2022-03-06 01:32:34 +0200

    h

--- js/apps/mail/cmps/side-bar.cmp.js
+++ js/apps/mail/cmps/side-bar.cmp.js
@@ -1,14 +1,13 @@
 import {mailService} from '../services/MailService.js';
-import percentageBar from './../cmps/percentage-bar.cmp.js'
+import percentageBar from './../cmps/percentage-bar.cmp.js' 
+// import percentageBar from '../../../../img/mail/logo.png'
 export default {
     props:['perc'],
     template: `
                 <div class="side-bar">
-                <div class="compose-btn" @click = "compose" >compose</div>
-                
-                <div class="folders">
                         <ul class="folder-list">
-                            <div class="divside">
+                            <div class="button-sidebar" @click = "compose" >compose</div>
+
                                 <li class="li" @click="inbox('inbox')">
                                     <div class="button-sidebar">Inbox</div>
                                 </li>
@@ -16,14 +15,15 @@ export default {
                                 <li class="li-sent" @click="sent('sent')">
                                     <div class="button-sidebar">Sent</div>
                                 </li>
-                            </div>
                             <!-- <li class="li">Drafts</li> -->
                             <li>
                                 <percentage-bar :percBar="perc" ></percentage-bar>
                             </li>
+
+                            <img class="mail-logo" src="../../../../img/mail/logo.png">
                             
                         </ul>
-                    </div>
+
                 </div>
     `,
     data() {
